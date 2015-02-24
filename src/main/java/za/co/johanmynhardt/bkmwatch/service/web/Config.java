5c17026fff281a103e05a999063c321334daaf0f
Johan Mynhardt <johanmynhardt@gmail.com>
2015-02-24 09:02:23 +0200

    Add DataSource config.

--- src/main/java/za/co/johanmynhardt/bkmwatch/service/web/Config.java
+++ src/main/java/za/co/johanmynhardt/bkmwatch/service/web/Config.java
@@ -1,14 +1,21 @@
 package za.co.johanmynhardt.bkmwatch.service.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
 
 /**
  * @author johan
@@ -17,6 +24,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 @ComponentScan("za.co.johanmynhardt.bkmwatch")
 public class Config extends WebMvcConfigurerAdapter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Config.class);
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -33,5 +42,11 @@ public class Config extends WebMvcConfigurerAdapter {
         return propertySourcesPlaceholderConfigurer;
     }
 
+    @Bean
+    public DataSource getDataSource(@Value("${dbUrl}") String dbUrl) {
+        DataSource dataSource = new DriverManagerDataSource(dbUrl);
+        LOG.debug("Set up dataSource for dbUrl={}", dbUrl);
+        return dataSource;
+    }
 
 }
