bf197e3e45467983299d7ff27eb176253f9fc62c
Johan Mynhardt <johanmynhardt@gmail.com>
2015-08-09 14:46:30 +0200

    Better UX and feedback in search view.

--- src/main/webapp/app/search/ctrl/search_controller.js
+++ src/main/webapp/app/search/ctrl/search_controller.js
@@ -3,9 +3,48 @@ app.controller('SearchController', function ($scope, $log, dbSearch) {
 
     $scope.items = [];
 
+    $scope.searchState = {
+        search: "",
+        searched: false,
+        searching: false,
+        searchDone: function() {
+            this.searched = true;
+            this.searching = false;
+        },
+        dirty: function(term) {
+            this.search = term;
+            this.searched = false;
+        },
+        setSearching: function() {
+            this.searching = true;
+            $scope.items = [];
+        }
+    };
+
+    $scope.$watch('search', function (newValue, old) {
+        $scope.searchState.dirty(newValue);
+    });
+
     $scope.doSearch = function () {
+
+        if ($scope.search.length <= 0) {
+            return;
+        }
+
         $log.debug("search for " + $scope.search);
 
-        $scope.items = dbSearch.query({search: $scope.search});
+        $scope.searchState.setSearching();
+
+        dbSearch.query(
+            {search: $scope.search},
+            function (data) {
+                $scope.searchState.searchDone();
+                $scope.items = data;
+            },
+            function (error) {
+                $log.error("Error searching for '%s': %o", $scope.search, error);
+                $scope.searchState.searchDone();
+            }
+        );
     }
 });
